e0fabba2c95caaff28ed7e03a935347a84ffe7ab
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-03-26 21:27:08 +0100

    Forrm example

--- index.js
+++ index.js
@@ -1,58 +1,13 @@
-//console.log(this);
+const form = document.getElementById('name-form');
 
-// alert('test'); // window.alert('test)
+form.addEventListener('submit', consoleForm);
 
-//console.dir(document);
+function consoleForm(event) {
+  event.preventDefault();
 
-/*
-
-1. Всі елементи на сторінці - окремі обʼєкти
-2. Всі елементи можна підписати під події
-
-*/
-
-function getAlert(event) {
-  const btn = event.target;
-
-  if (btn.style.backgroundColor === red) {
-    btn.style.backgroundColor = 'unset';
-  } else {
-    btn.style.backgroundColor = 'red';
-  }
+  const form = event.target;
+  const value = form[0].value;
+  console.log(value);
 }
 
-const buttonElement = document.getElementById('btn');
 
-buttonElement.addEventListener('click', getAlert);
-// 1 варіант: зареєстрували обробник  події натискання миші на кнопку
-// 2 варіант: навісили обробник натискання миші на кнопку
-
-/*
-
-Зробіть кнопку, додайте id
-Напишіть функцію, яка виводить в консоль "привіт"
-Навісити обробку події наведення миші на цю кнопку
-
-*/
-
-const secondButton = document.getElementById('second-btn');
-
-function consoleHello() {
-  console.log('Привіт');
-}
-
-secondButton.addEventListener('mouseenter', consoleHello);
-
-// При наведені на кнопку ні - потрібно її приховати (display: 'none')
-
-const noBtn = document.getElementById('no-btn');
-
-function ghostBtn(event) {
-//   const btn = event.target;
-//   btn.style.display = 'none';
-
-  event.target.removeEventListener('mouseenter', ghostBtn);
-  console.log('test');
-}
-
-noBtn.addEventListener('mouseenter', ghostBtn);
