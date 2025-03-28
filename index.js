c4dedecd79f25c9dc5e27e979535004f1384f0f0
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-03-28 20:41:21 +0100

    COnvector example

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
-const btn = document.querySelector('button');
+/*
+Завдання: створити конвертер валют
 
-const btn2 = document.querySelector('.super-btn');
+Декомпозиція:
 
-const btn3 = document.querySelector('#btnbtn');
+1. Створити html-сторінку  формою, яка містить два поля вводу:
+одне поле для введення суми в оній валюті
+інше поле - для вибору валюти, в яку ми будемо конвертувати
 
-const btnCollection = document.querySelectorAll('button');
+2. Додати в форму кнопку "Конвертувати"
 
-const paragraphCollection = document.querySelectorAll('.paragraph:nth-child(even)')
+3. Написати в js-обробник події відправки форми:
+- отримати значення введеної суми та обраної валюти
+- обчислити конвертовану суму, помноживши введену користувачем суму на курс обміну вибраної користувачем валюти
+- вивести результат конвертації на сторінку для відображення результату (innrHTML)
 
-for(let p of paragraphCollection){
-    p.style.backgroundColor = 'yellow'
+4. Підписати форму на подію "submit", в якості лісенера використовувати функцію з п. 3
+
+
+*/
+
+const CURRENCY = {
+  USD_TO_UA: 41.25,
+  EUR_TO_UA: 44.87,
+};
+
+const converterForm = document.querySelector('#currency-converter-form');
+
+converterForm.addEventListener('submit', convertHandler);
+
+function convertHandler(event) {
+  event.preventDefault();
+
+  const amount = Number(document.querySelector('#amount').value);
+  const currency = document.querySelector('#currency').value;
+
+  let convertedAmount;
+
+  const { USD_TO_UA: usdCours, EUR_TO_UA: eurCours } = CURRENCY;
+
+  switch (currency) {
+    case 'USD': {
+      convertedAmount = amount * usdCours;
+      break;
+    }
+    case 'EUR': {
+      convertedAmount = amount * eurCours;
+      break;
+    }
+    default:
+      throw 'Something problem.....!';
+  }
+
+//   if (currency === 'USD') {
+//     convertedAmount = amount * usdCours;
+//   } else if (currency === 'EUR') {
+//     convertedAmount = amount * eurCours;
+//   } else {
+//     throw 'Something problem.....!';
+//   }
+
+  const resuldDiv = document.querySelector('#result');
+  resuldDiv.innerHTML = `${amount} ${currency} = ${convertedAmount.toFixed(
+    2
+  )} UAN`;
 }
