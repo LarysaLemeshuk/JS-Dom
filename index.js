d79ecf7ad11d0963f3e0bc41ae025e77c6ec6f0b
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-03-31 20:33:43 +0200

    Example for remembering

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-/*
-Створити кнопку, яка буде змінювати тему сайту(html)
+// const event = new Event('click');
+// console.log(event.composedPath());
 
-js. Якщо ви натискаєте  на цю кнопку - на сайті вмикається темний режим
-(дляя тегу body встановіть backgroundColor = якийсь_темний_колір
-color: white
+const button = document.querySelector('button');
 
-Якщо ви натискаєте на цю кнопку ще раз - на сайті вимикається темний режим (toggle)
-*/
+button.addEventListener('click', btnCLickHandlrr);
 
-const themeToggleBtn = document.querySelector('#theme-toggle');
-const body = document.body;
-
-
-themeToggleBtn.addEventListener('click', toggleTheme)
-
-function toggleTheme(){
-body.classList.toggle('dark-theme')
+function btnCLickHandler(event) {
+  console.log('hi from buttton handler');
+  // console.log(event.composedPath());
+  console.log(event.tareget); // той, на кому спрацювала подія
+  // target - елемент, на якому сталась подія
+  // target - елемент, доя кого буде подія занурюватись
+  console.log(event.currentTarget); // той, кому належить eventListener
+  //currentlarget - елемент, якому належав обробник подій
 }
+
+document.body.addEventListener('click', bodyClickHandler);
+
+function bodyClickHandler(event) {
+  console.log('hi from body click handler');
+  console.log(event.tareget); // той, на кому спрацювала подія
+  console.log(event.currentTarget); // той, кому належить eventListener
+}
+
+const clickEvent = new MouseEvent('click');
+
+// button.dispatchEvent(clickEvent);
+
+/*
+3 фази події:
+
+1. Фаза занурення.
+Подія стається на рівні ОС, ОС передає подію браузеру (Window), той передає подію document -> body ->.........-> елемент, на якому сталась подія
+
+2. Фаза цілі.
+Подія досягла елемента, елемент - це таргет
+
+3. Фаза сплиття.
+Подія починає спливати у зворотньому напрямку, тобто від елемента (таргета) до ОС
+*/
