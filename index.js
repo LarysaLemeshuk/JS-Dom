024dbf6f6c81ae61fd2d7888050e6ca0c0227317
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-04-02 14:15:24 +0200

    Img src task resolve

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 /*
+1.В HTML маємо елемент img
+2. В HTML маємо дві кнопки
+Ці кнопки в data-атрибутах містять постлання на певну картинку(адреса картинки)
+3. Задача: за натисненням на кнопку має відобразитись посилання в тезі img відповідна цій кнопці картинка
 
-1. Ми маємо div
-2. Маємо 5 кнопок, на кожній з яких написаний колір
-3. Задача: за натисненням на кнопку, фоновий колір діва має змінитися на відповідний колір, вказаний на кнопці
-
+ https://innovet.com.ua/wp-content/uploads/2020/07/afade8d0b4aa9967ddac510a785914a3.jpg
+https://wikizoo.ru/upload/iblock/64b/7t5t1vfph4d0c2m3x5qs5gsxgbx6wdsu.jpg
 */
+const [btn1, btn2] = document.querySelectorAll('button');
+//console.log(btns);
 
-const buttons = document.querySelectorAll('button');
-const div = document.querySelector('#root');
+btn1.addEventListener('click', clickHandler);
+btn2.addEventListener('click', clickHandler);
 
-console.dir(buttons);
-
-// for (let i = 0; i < buttons.clickHandler; i++) {
-//   buttons[i].addEventListener('click', clickHandler);
-// }
-
-for (let btn of buttons) {
-  btn.addEventListener('click', clickHandler);
-}
-
-function clickHandler({ target:{dataset:{color}, parentNode} }) {
-//   const {
-//     dataset: { color },
-//   } = target;
-//   const { parentNode } = target;
-  parentNode.style.backgroundColor = color;
+function clickHandler(target) {
+  const img = target.parentNode.children[0];
+  img.setAtribute('src', target.dataset.src);
 }
