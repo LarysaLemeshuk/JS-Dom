c13f6240224d3860800644bfa6ab0576f1d154af
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-04-02 14:41:55 +0200

    Create element

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-/*
-1.В HTML маємо елемент img
-2. В HTML маємо дві кнопки
-Ці кнопки в data-атрибутах містять постлання на певну картинку(адреса картинки)
-3. Задача: за натисненням на кнопку має відобразитись посилання в тезі img відповідна цій кнопці картинка
+function createDiv() {
+  const div = document.createElement('div'); // 1. Створюємо елемент у памʼяті
+  div.classList.add('box')// 2. До цього елементу приєднуємо стильовий клас
+  div.append('Hello world'); // 3. До цього елементу приєднуємо текстовий вузол
+  console.log(div);
 
- https://innovet.com.ua/wp-content/uploads/2020/07/afade8d0b4aa9967ddac510a785914a3.jpg
-https://wikizoo.ru/upload/iblock/64b/7t5t1vfph4d0c2m3x5qs5gsxgbx6wdsu.jpg
-*/
-const [btn1, btn2] = document.querySelectorAll('button');
-//console.log(btns);
+  document.body.append(div); // 4. ПРиєднуємо створений елемент до body
 
-btn1.addEventListener('click', clickHandler);
-btn2.addEventListener('click', clickHandler);
-
-function clickHandler(target) {
-  const img = target.parentNode.children[0];
-  img.setAtribute('src', target.dataset.src);
 }
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', createDiv)
+
+
+/*
+
+Спробуйте створити декілька HTML елементів через JS. (document.createElement)
+Спробуйте додати якісь стильові класи цим елементам. (element.classlist.add)
+Спробуйте додати якийсь текстовий вузол для елементів. ( element append( 'text.... ')
+Причепіть створені елементи до body ( document.body append (element)
+
+*/
