5171f1989a3992961a4ab4106e060a1fff3b130e
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-04-08 17:01:08 +0200

    Refactor

--- index.js
+++ index.js
@@ -10,10 +10,7 @@
 const root = document.querySelector('#root');
 
 function createUserCard(user) {
-  // створення article
-  const article = document.createElement('article');
-  article.classList.add('card-wrapper');
-
+  
   // створення img
   const img = document.createElement('img');
   img.setAttribute('src', user.profilePicture);
@@ -21,22 +18,37 @@ function createUserCard(user) {
   img.classList.add('card-image');
 
   // створення h2
-  const h2 = document.createElement('h2');
-  h2.append(user.name);
-  h2.classList.add('username');
+  const h2 = createElement('h2', { classNames: ['username'] }, user.name);
 
   // створення p
-  const p = document.createElement('p');
-  p.append(user.description);
-  p.classList.add('description');
+  const p = createElement(
+    'p',
+    { classNames: ['description'] },
+    user.description
+  );
 
-  // приєднати до article елементи створенні в п 2-4(img, h2, p)
-  article.append(img, h2, p);
-
-  // повертаємо створений article
+  // створення і повернення article, в якй вкладені створені елементи
+  const article = createElement('article', { classNames: ['card-wrapper'] }, img, h2, p);
   return article;
+  
 }
 
 const cardArray = data.map((user) => createUserCard(user));
 
 root.append(...cardArray);
+
+/**
+ *
+ * @param {String} type - тег елемента, який потрібно створити
+ * @param {String []} classNames - список класів, які потрібно додати до елемента
+ * @param  {...Node} chilNodes - список дочірніх вузлів
+ * @returns {HTML Element}
+ */
+
+function createElement(type, { classNames }, ...chilNodes) {
+  const elem = document.createElement(type);
+  elem.classList.add(...classNames);
+  elem.append(...chilNodes);
+
+  return elem;
+}
