0ac4ee8b7c4bae17dab4a28e07e08e73125a51ad
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-04-28 20:06:36 +0200

    Then, catch, finally

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-// fetch('./user.json')
-//   .then((response) => {
-//     return response.json(); // перетворюємо нулі та одинички на JS обʼєкт
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   });
-
-fetch('https://api.monobank.ua/bank/currency')
+fetch('./uer.json')
   .then((response) => {
     return response.json(); // перетворюємо нулі та одинички на JS обʼєкт
   })
   .then((data) => {
     console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log('FINALLY question closed');
   });
-
