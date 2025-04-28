8bb6de745e87f0e3d5ae6bf32cf3c4265d3fed3b
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-04-28 21:13:44 +0200

    setTimeOut in Promise

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-const p = new Promise(executor); // peding
+const p = new Promise(executor); // pending
 
 function executor(resolve, reject) {
-  // задача: згенерувати випадкове ціле число від 0 до 15
-  const number = Math.floor(math.random() * 15);
-
-  // якщо число кратне 2 - резолвимо проміс та повертаємо це число
-  // якщо число не кратне 2 - реджектимо проміс та повертаємо помилку
-
-  if (number % 2 === 0) {
-    resolve(number); // fulfilled
-  } else {
-    const err = new RangeError('Error happenned: ${number}');
-    reject(err);
-  }
+  setTimeout(() => {
+    resolve('It time'); // fulfielled
+  }, 5000);
 }
 
-p.then(
-  (number) => {
-    console.log(number);
-  },
-  (error) => {
-    console.log(error);
-  }
-);
+const h1 = document.querySelector('#root');
+
+p.then((str) => {
+  h1.append(str);
+});
