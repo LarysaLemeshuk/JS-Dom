6caae8afae1c712d8ff0c47748b0389e3dba8127
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-05-03 17:22:48 +0200

    weather widget( need refactor)

--- index.js
+++ index.js
@@ -1,52 +1,84 @@
-function loadImage(src) {
-  // створюємо саму картинку
-  const img = document.createElement('img');
-  imd.setAttribute('src', src);
+/*
+https://api.openweathermap.org/data/2.5/weather?q={Dnipro}&appid={f7c576ba3699bdd0b98ddcf196639992}
 
-  // створюємо елемент у верстці, який буде слугувати нам для того, що вказує, чи вантажимо ми щось чи ні
-  const h2 = document.createElement('h2');
-  h2.id = 'loading-h2';
-  h2.append('Loading image.....');
-  document.body.append(h2);
+f7c576ba3699bdd0b98ddcf196639992
 
-  // повертаємо проміс (промісифікаціям - коли ми огортажмо якийсь код в проміс, для того щоб забезпечити собі зручну роботу)
-  return new Promise((resolve, reject) => {
-    // створюємо таймаут на 5 сек. для завантаження картинки
-    const timeoutId = setTimeout(() => {
-      reject('Image can`t be loaded');
-    }, 5000);
+// Задача: зробити погодний віджет
 
-    // підписуємо створену картинку на подію лоад
-    // якщо картинка завантажиться _ ми резолвимо проміс з елементом картинки
-    img.addEventListener('load', () => {
-      resolve(img);
-    });
+Алгоритм вирішення:
 
-    // підписуємо створену картинку на подію ерор
-    // якщо картинка не завантажується - ми реджектимо з повідомленням про помилку
-    img.addEventListener('error', () => {
-      clearTimeout(timeoutId);
-      reject('Image can`t be loaded');
-    });
-  });
+
+1. Зробити верстку елементів, які отримують від користувача дані про місто
+
+2. Отримати дані з апі і обробити їх( підготувати дані для відмалювання у верстці)
+
+3. Зробити картку з погодою і відобразити її
+*/
+
+const API_KEY = 'f7c576ba3699bdd0b98ddcf196639992';
+const API_BASE = 'https://api.openweathermap.org/data/2.5/weather';
+
+const btn = document.querySelector('btn');
+
+btn.addEventListener('click', buttonClickHandler);
+
+function buttonClickHandler({ target }) {
+  const selectValue = target.previousElementSibling.value;
+  requestAPI(selectValue);
 }
 
-loadImage(
-  'https://zoobonus.ua/storage/breeds/images/big/VgPIiISj7OizrLf5V6EL8RyXdLldPgLlCNRG02Gl.jpg'
-)
-  .then(
-    (img) => {
-      document.body.append(img);
-      const loadingH2 = document.querySelector('#loading-h2');
-      loadingH2.remove();
-    },
-    (errorMessage) => {
-      const h2 = document.createElement('h2');
-      h2.append(errorMessage);
-      document.body.append(h2);
-    }
-  )
-  .finally(() => {
-    const loadingH2 = document.querySelector('#loading-h2');
-    loadingH2.remove();
-  });
+function requestAPI(cityName) {
+  // готуєм url
+  const url = `${API_BASE}?q=${cityName}&appid=${API_KEY}$units=metric`;
+  console.log(url);
+
+  // робимо запит
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      // відмальовуємо погоду
+      displayWeather(data);
+    });
+}
+
+/*
+
+  <article class="weather">
+        <p>City name: Kyiv</p>
+        <p>Temperature: 7°C</p>
+        <p>Weather description: overcast clouds</p>
+      </article>
+*/
+
+function displayWeather(weatherObject) {
+  const {
+    name,
+    main: { temp },
+    weather: [{ description }],
+  } = weatherObject;
+
+  // створюємо article
+  const article = document.createElement('article');
+  article.classList.add('weather');
+
+  // створюємо параграф з назвою міста
+  const cityName = document.createElement('p');
+  cityName.append('City name: ${name) ');
+
+  // створюємо параграф з температурою
+  const temperature = document.createElement('P');
+  temperature.append(`Temperature: ${temp}°C`);
+
+  // створюємо параграф з описом погоди
+  const weatherDescription = document.createElement('p');
+  weatherDescription.append(`Weather description: ${description}`);
+
+  // до article чіпляємо параграфи створені в п. 2-4
+  article.append(cityName, temperature, weatherDescription);
+
+  // знаходимо секцію і чіпляємо до неї article
+  const section = document.querySelector('.wrapper');
+  section.append(article);
+}
